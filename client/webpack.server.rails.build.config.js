229726b3de395ec54ca6e6489cb463ecd997ccfb
Michael Westbom <michael@agilion.com>
2016-01-25 14:12:32 -0500

    Finish server rendering stuff

--- client/webpack.server.rails.build.config.js
+++ client/webpack.server.rails.build.config.js
@@ -10,6 +10,44 @@ module.exports = {
   context: __dirname,
   entry: [
     'babel-polyfill',
-    './app/bundles/HelloWorld/startup/clientRegistration.jsx'
-  ]
-}
+    './app/bundles/HelloWorld/startup/serverRegistration'
+  ],
+  output: {
+    filename: 'server-bundle.js',
+    path: '../app/assets/webpack'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      libs: path.join(process.cwd(), 'app', 'libs'),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        loaders: [
+          'css/locals?modules&importLoaders=0&localIdentName=[name]__[local]__[hash:base64:5]',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'css/locals?modules&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:5]',
+          'sass',
+          'sass-resources',
+        ],
+      },
+    ],
+  },
+
+  sassResources: ['./app/assets/styles/app-variables.scss'],
+};
