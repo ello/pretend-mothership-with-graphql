dc0b34bd1d0ec651cbeb1a487c1a78caadb784ab
Michael Westbom <michael@agilion.com>
2016-01-29 13:06:59 -0500

    Comments

--- app/graph/node_identification.rb
+++ app/graph/node_identification.rb
@@ -1,9 +1,13 @@
 NodeIdentification = GraphQL::Relay::GlobalNodeIdentification.define do
-  object_from_id -> (id, ctx) do
+  # Given a UUID and the query context,
+  # return the corresponding application object
+  object_from_id -> (id, _ctx) do
     type, id = NodeIdentification.from_global_id(id)
     Object.const_get(type).find(id)
   end
 
+  # Given an application object,
+  # return a GraphQL ObjectType to expose that object
   type_from_object -> (object) do
     (object.class.name + 'Type').constantize
   end
